3533e80368dad7f25862279c43b18b49e97eca48
Pavel Anokhin <retyui55@gmail.com>
2021-11-17 13:37:33 +0300

    pattern of bot

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
-import aiogram
+#!venv/bin/python3.9
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+import config
 
 
-def main():
-    print("hello!")
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
 
+# /test1
+async def cmd_test1(message: types.Message):
+    await message.reply("Test1")
+
+dp.register_message_handler(cmd_test1, commands="test1")
 
 if __name__ == "__main__":
-    main()
+    executor.start_polling(dp, skip_updates=True)
 
